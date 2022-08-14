fe4280420e8cb04e9279f866eeaf27e357e6789b
Giovani Rodrigues <giovani.srodrigues@hotmail.com>
2022-08-14 14:44:09 -0300

    appBarConfig / navController

--- home/src/main/java/br/com/tournaments/home/HomeActivity.kt
+++ home/src/main/java/br/com/tournaments/home/HomeActivity.kt
@@ -2,25 +2,37 @@ package br.com.tournaments.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import br.com.tournaments.home.databinding.ActivityHomeBinding
 import br.com.tournaments.home.presentation.home.HomeViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     private val viewModel by viewModel<HomeViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        val binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        viewModel.getMatches()
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.homeNavHostFragment) as NavHostFragment
 
-        viewModel.itens.observe(this) { teams ->
-            teams.map {
-                Log.i("adriano", it.transform().name)
-            }
+        navController = navHostFragment.navController
 
-        }
+        binding.bottomNavHome.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment)
+        )
+
+        binding.toolbarApp.setupWithNavController(navController, appBarConfiguration)
+
     }
 }
